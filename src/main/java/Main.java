ddc469bc2b64425543233d937345d387fe809ce1
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-09-21 00:01:46 -0300

    Added Spark dependency.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import org.json.simple.JSONObject;
+import static spark.Spark.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		final JSONObject obj = new JSONObject();
+		obj.put("name", "foo");
+		obj.put("num", new Integer(100));
+		System.out.println(obj);
+		
+		
+		get("/read", (req, res) -> obj);
 	}
 	
 }
